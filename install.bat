549f8f1aa5f78203f38dee2ab1d85257c0095495
Dmitry Kuvshinov <dmitryk@genustechnologies.com>
2022-10-17 09:52:00 +0300

    Initial commit

---
+++ install.bat
@@ -0,0 +1,25 @@
+rem call npx expo install expo-intent-launcher
+rem call npx expo install expo-file-systemcall i @react-native-async-storage/async-storage
+rem call i @react-navigation/native
+rem call i @react-navigation/drawer
+call i    @reduxjs/toolkit
+rem call i    moment
+rem call i    react-native-base64
+rem call i    react-native-gesture-handler
+rem call i    react-native-popup-menu
+rem call i    react-native-reanimated
+rem call i    react-native-safe-area-context
+rem call i @react-navigation/bottom-tabs
+rem call i    react-native-screens
+call i    react-redux
+call i    redux
+call npm install react-native-loading-spinner-overlay
+rem call i @react-navigation/native-stack
+rem call npm install @react-navigation/drawer
+rem call npx expo install react-native-gesture-handler react-native-reanimated
+rem call npx expo install react-native-screens react-native-safe-area-context
+rem call i react-native-bouncy-checkbox
+
+rem this may not work
+REM npm install @react-native-picker/picker  --legacy-peer-deps
+REM npm install react-native-dropdown-picker
